4d2988cecbcf581b8a931d2e6dea71f23a2d28e8
Ronan Guilloux <ronan.guilloux@gmail.com>
2013-01-15 14:46:59 +0100

    changing port to dual use other http servers while demo-ing

--- run.js
+++ run.js
@@ -3,4 +3,4 @@ var connect = require('connect')
     , serverSignature = 'Node.js / Debian ' + [os.type(), os.release(), os.arch()].join(' ') + ' / Raspberry Pi';
 console.log(serverSignature,os.networkInterfaces().wlan0);
 console.log('running... CTRL+C to stop');
-return connect.createServer(connect.static(__dirname)).listen(8080);
+return connect.createServer(connect.static(__dirname)).listen(8081);
