f99a86db333adf254c123a7c49984b3cdce22ce6
Ronan Guilloux <ronan.guilloux@gmail.com>
2013-01-15 04:34:03 +0100

    node server made verbose

--- run.js
+++ run.js
@@ -1,2 +1,6 @@
-var connect = require('connect');
-connect.createServer(connect.static(__dirname)).listen(8080);
+var connect = require('connect')
+    , os = require('os')
+    , serverSignature = 'Node.js / Debian ' + [os.type(), os.release(), os.arch()].join(' ') + ' / Raspberry Pi';
+console.log(serverSignature,os.networkInterfaces().wlan0);
+console.log('running... CTRL+C to stop');
+return connect.createServer(connect.static(__dirname)).listen(8080);
